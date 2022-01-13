196a999d59a5479d4d0dbce05d12b26350cce6cb
stoneboat <tood2111@gmail.com>
2022-01-14 00:55:23 +0900

    clone

---
+++ einsmedia/script/main.js
@@ -0,0 +1,9 @@
+const navbar = document.querySelector(".header-cont");
+const navbarHeight = navbar.getBoundingClientRect().height;
+document.addEventListener("scroll", () => {
+  if (window.scrollY > navbarHeight) {
+    navbar.classList.add("navbar--dark");
+  } else {
+    navbar.classList.remove("navbar--dark");
+  }
+});
